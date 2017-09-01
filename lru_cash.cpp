e765956f4e4a4323aa0146c3ccea41b16e460dca
Yan Bezborodov <bezborodov30@yandex.ru>
2017-09-01 19:57:00 +0300

    other solution starts here

--- lru_cash.cpp
+++ lru_cash.cpp
@@ -1,18 +1,85 @@
 #include <bits/stdc++.h>
 
-struct lru_cache
+struct node
 {
 public:
+    node() : left(nullptr), right(nullptr), parent(nullptr), next(nullptr), prev(nullptr) {}
+    node(node* left, node* right, node* parent, node* next, node* prev) : 
+    left(left), right(right), parent(parent), next(next), prev(prev) {}
+
+    node*       left;
+    node*       right;
+    node*       parent;
+
+    node*       next;
+    node*       prev;
+};
+
+template<typename T, typename U>
+struct node_with_data : node
+{
+public:
+    typedef T key_type;
+    typedef U mapped_type;
+    typedef std::pair<key_type, mapped_type> value_type;
+    value_type val;
+    node_with_data(value_type const& val) : val(val) {}
+};
+
+template<typename T, typename U>
+struct lru_cache
+{
+private:
+    node* end_; // Наибольший по значению элемент
+    size_t sz, capacity;
+public:
     // Вы можете определить эти тайпдефы по вашему усмотрению.
-    typedef int key_type;
-    typedef int mapped_type;
+    typedef T key_type;
+    typedef U mapped_type;
     typedef std::pair<key_type, mapped_type> value_type;
 
     // Bidirectional iterator.
-    struct iterator;
+    struct iterator
+    {
+        // Элемент на который сейчас ссылается итератор.
+        // Разыменование итератора end() неопределено.
+        // Разыменование невалидного итератора неопределено.
+        value_type const& operator*() const {
+            try {
+                return static_cast<node_with_data<T, U>*>(v)->val;
+            } catch(...) {
+                std::cerr << "can't use operator '*' with end() iterator\n";
+                throw;
+            }
+        }
+
+        // Переход к элементу со следующим по величине ключом.
+        // Инкремент итератора end() неопределен.
+        // Инкремент невалидного итератора неопределен.
+        iterator& operator++();
+        iterator operator++(int);
+
+        // Переход к элементу со следующим по величине ключом.
+        // Декремент итератора begin() неопределен.
+        // Декремент невалидного итератора неопределен.
+        iterator& operator--();
+        iterator operator--(int);
+        
+        friend bool operator==(const iterator& a, const iterator& b) {
+            return a.v == b.v;
+        }
+        friend bool operator!=(const iterator& a, const iterator& b) {
+            return a.v != b.v;
+        }
+        
+        friend lru_cache;
+        
+    private:
+        node* v;
+    };
 
     // Создает пустой lru_cache с указанной capacity.
-    lru_cache(size_t capacity) : sz(0), capacity(capacity), root_(nullptr), begin_(nullptr) {}
+    lru_cache(size_t capacity);
 
     // Деструктор. Вызывается при удалении объектов lru_cache.
     // Инвалидирует все итераторы ссылающиеся на элементы этого lru_cache
@@ -43,152 +110,11 @@ public:
     iterator begin() const;
     // Возващает итератор на элемент следующий за элементом с максимальным ключом.
     iterator end() const;
-private:
-    struct node
-    {
-    public:
-        node() {}
-        node(value_type key_mapped) :
-                key_mapped(key_mapped), left(nullptr), right(nullptr),
-                parent(nullptr), next(nullptr), prev(nullptr) {}
-        value_type key_mapped;
-
-        node*       left;
-        node*       right;
-        node*       parent;
-
-        node*       next;
-        node*       prev;
-    };
-    node *root_, *begin_;
-    size_t sz; // current size of lru_cache
-    size_t capacity; // maximal size
-    friend iterator;
-};
-
-struct lru_cache::iterator
-{
-private:
-    node* value;
-public:
-    iterator() : value(nullptr) {}
-    iterator(node* value) : value(value) {}
-    // Элемент на который сейчас ссылается итератор.
-    // Разыменование итератора end() неопределено.
-    // Разыменование невалидного итератора неопределено.
-
-    value_type const& operator*() const {
-        return value->key_mapped;
-    }
-
-    iterator(iterator const& other) {
-        this->value = other.value;
-    }
-
-
-
-    // Переход к элементу со следующим по величине ключом.
-    // Инкремент итератора end() неопределен.
-    // Инкремент невалидного итератора неопределен.
-    iterator& operator++() {
-        if (value->right) {
-            value = value->right;
-            while (value->left) {
-                value = value->left;
-            }
-            return *this;
-        }
-        value = value->parent;
-        if (!value) return *this;
-        while (value->right) {
-            value = value->right;
-        }
-        return *this;
-    }
-
-    iterator operator++(int) {
-        iterator tmp = *this;
-        ++(*this);
-        return tmp;
-    }
-
-    // Переход к элементу со следующим по величине ключом.
-    // Декремент итератора begin() неопределен.
-    // Декремент невалидного итератора неопределен.
-    iterator& operator--() {
-        if (value->left) {
-            value = value->left;
-            while (value->right) { value = value->right; }
-            return *this;
-        }
-        node* cur = value->parent;
-        while (cur && value == cur->left) {
-            value = cur;
-            cur = cur->parent;
-        }
-        value = cur;
-        return *this;
-    }
-
-    iterator operator--(int) {
-        iterator tmp = *this;
-        --(*this);
-        return *this;
-    }
-
-    friend bool operator==(iterator, iterator);
-    friend bool operator!=(iterator, iterator);
 };
 
 // Сравнение. Итераторы считаются эквивалентными если они ссылаются на один и тот же элемент.
 // Сравнение с невалидным итератором не определено.
 // Сравнение итераторов двух разных контейнеров не определено.
-bool operator==(lru_cache::iterator a, lru_cache::iterator b) {
-    return a.value == b.value;
-}
-bool operator!=(lru_cache::iterator a, lru_cache::iterator b) {
-    return !(a == b);
-}
-
-lru_cache::iterator lru_cache::begin() const {
-    return iterator(begin_);
-}
-
-lru_cache::iterator lru_cache::end() const {
-    return iterator(begin_->prev);
-}
-
-lru_cache::iterator lru_cache::find(key_type key) {
-        node *cur = root_;
-        while (cur) {
-            if (cur->key_mapped.first < key) cur = cur->right;
-            else if (cur->key_mapped.first > key) cur = cur->left;
-            else break;
-        }
-
-        if (!cur) {
-//            return end();
-        }
-
-        cur->prev->next = cur->next;
-        cur->next->prev = cur->prev;
-
-        node *&last = begin_->prev;
-        cur->prev = last;
-        last->next = cur;
-        cur->next = begin_;
-        begin_->prev = cur;
-
-        return iterator(cur);
-}
-
-void lru_cache::erase(iterator) {}
-
-lru_cache::~lru_cache() {
-    while (sz > 0) {
-        erase(begin());
-    }
-}
 
 int main() {
     return 0;
