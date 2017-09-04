1ffd3c7ddc7b91df84ba76051f070cc9cfd8bebf
Alexander <alexander@ubuntu.ubuntu-domain>
2017-09-04 22:34:40 +0300

    tests added

--- list.cpp
+++ list.cpp
@@ -2,6 +2,9 @@
 #include <cassert>
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
 template<typename T>
 struct list {
 public:
@@ -388,6 +391,62 @@ void print(list<T>& l) {
     std::cout << '\n';
 }
 
+list<int> f(int start, int finish) {
+    list<int> res;
+    while (start != finish) {
+        res.push_back(start);
+        start++;
+    }
+    return res;
+}
+
+void Alexander_check() {
+    list<int> c;
+    c.push_back(1);
+    c.pop_front();
+    c.push_back(2);
+    c.pop_front();
+    for (int i = 0; i < 5; ++i) c.push_back(i);
+    list<int> b;
+    b.push_back(9);
+    b.push_back(10);
+    auto it = c.end();
+    auto iit = b.end();
+    c.splice(--(--it), b, b.begin(), --iit);
+    --(--it);
+    auto pit = it;
+    it = c.erase(it);
+    it = c.erase(c.erase(it));
+    cout << *it << endl;
+    c.insert(it, 11);
+//    c.clear();
+    list<int> d;
+    d = c;
+    b.push_front(12);
+    b.pop_back();
+//    b.pop_back();
+//    b.pop_front();
+//    cout << *it << endl;
+    d.splice(d.begin(), b, b.begin(), b.end());
+//    d.splice(c.begin(), b, b.begin(), b.end());
+    std::cout << "OK" << std::endl;
+    for (auto iit = d.rbegin(); iit != d.rend(); ++iit) {
+        cout << *iit << " ";
+    }
+    cout << endl;
+    for (auto it = b.begin(); it != b.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+    cout << *it << endl;
+
+    c = f(20, 40);
+    for (auto i : c) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     list<int> x;
     //*
@@ -429,7 +488,7 @@ int main() {
         x.pop_front();
         x.pop_back();
     }
-    
+
     for (int i = 0; i < 10; ++i)
         x.push_front(i);
     for (int i = 0; i < 10; ++i)
@@ -471,5 +530,8 @@ int main() {
     while (!x.empty()) {
         x.pop_front();
     }
+
+    Alexander_check();
     return 0;
 }
+
